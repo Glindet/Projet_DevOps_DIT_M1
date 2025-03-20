c9a436d2286c80715210fb10a6914cbf3eaef336
Alex0991 <yapo991@gmail.com>
2025-03-20 21:16:15 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'todo-index'),
-    path('update/<int:pk>/', views.update, name = 'todo-update'),
-    path('delete/<int:pk>/', views.delete, name = 'todo-delete'),
-
-]
